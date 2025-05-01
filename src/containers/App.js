0d11162abed097b149d2884fe2e1efbb0d5a5ba9
widzthedvloper <widzmarc@gmail.com>
2025-04-30 22:00:57 -0400

    Removing local state variable

--- src/containers/App.js
+++ src/containers/App.js
@@ -23,7 +23,6 @@ class App extends Component {
     super()
     this.state = {
       robots: [],
-      searchfield: ''
     }
   }
 
@@ -33,21 +32,18 @@ class App extends Component {
       .then(users => {this.setState({ robots: users})});
   }
 
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value })
-  }
-
   render() {
-    const { robots, searchfield } = this.state;
+    const { robots } = this.state;
+    const {searchField, onSearchChange} = this.props;
     const filteredRobots = robots.filter(robot =>{
-      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
     })
     return !robots.length ?
       <h1>Loading</h1> :
       (
         <div className='tc'>
           <h1 className='f1'>RoboFriends</h1>
-          <SearchBox searchChange={this.onSearchChange}/>
+          <SearchBox searchChange={onSearchChange}/>
           <Scroll>
             <CardList robots={filteredRobots} />
           </Scroll>
